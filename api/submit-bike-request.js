7693cc284d7cfea9e5f48856487b0b76139002e6
muhamadharis-khld <muhamad.haris@imotorbike.com>
2026-05-25 11:16:08 +0800

    Update submit-bike-request.js

--- api/submit-bike-request.js
+++ api/submit-bike-request.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method not allowed"
@@ -62,4 +62,4 @@ ${notes || "-"}`
       error: error.message
     });
   }
-}
+};
